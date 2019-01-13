858a36e350c6d104524055d0c4db3e43b911bc05
limamauricio <mauriciolimab@gmail.com>
2019-01-13 17:22:29 -0300

    add interface to handle movie click

---
+++ app/src/main/java/com/limamauricio/popularmovies/utils/MovieOnClickListener.java
@@ -0,0 +1,8 @@
+package com.limamauricio.popularmovies.utils;
+
+import com.limamauricio.popularmovies.model.Movie;
+
+public interface MovieOnClickListener {
+
+    void onMovieClick(Movie movie);
+}
