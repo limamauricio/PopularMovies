944f18dc5133a02d358b4c81752fea6da057af84
limamauricio <mauriciolimab@gmail.com>
2019-02-05 05:39:13 -0300

    add getTrailersById call

--- app/src/main/java/com/limamauricio/popularmovies/proxy/Proxy.java
+++ app/src/main/java/com/limamauricio/popularmovies/proxy/Proxy.java
@@ -1,9 +1,11 @@
 package com.limamauricio.popularmovies.proxy;
 
 import com.limamauricio.popularmovies.model.MoviesRequestResponse;
+import com.limamauricio.popularmovies.model.TrailersRequestResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface Proxy {
@@ -16,5 +18,9 @@ public interface Proxy {
     @GET("movie/top_rated")
     Call<MoviesRequestResponse> getTopMovies(@Query("page") int page,
                                                  @Query("api_key") String api);
+
+    @GET("movie/{id}/videos")
+    Call<TrailersRequestResponse> getTrailers(@Path("id") int id,
+                                              @Query("api_key") String api);
 
 }
