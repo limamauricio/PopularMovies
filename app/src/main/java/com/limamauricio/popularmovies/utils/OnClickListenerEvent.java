6f5c9cdf257958223f12a0372bcdd38013e17be5
limamauricio <mauriciolimab@gmail.com>
2019-02-05 05:39:48 -0300

    add onTrailerClick event

--- app/src/main/java/com/limamauricio/popularmovies/utils/OnClickListenerEvent.java
+++ app/src/main/java/com/limamauricio/popularmovies/utils/OnClickListenerEvent.java
@@ -1,8 +1,11 @@
 package com.limamauricio.popularmovies.utils;
 
 import com.limamauricio.popularmovies.model.Movie;
+import com.limamauricio.popularmovies.model.Trailer;
 
-public interface MovieOnClickListener {
+public interface OnClickListenerEvent {
 
     void onMovieClick(Movie movie);
+
+    void onTrailerClick(Trailer trailer);
 }
