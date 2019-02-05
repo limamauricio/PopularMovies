1f3665734a8346533e8cc6275a450c879b5978fd
limamauricio <mauriciolimab@gmail.com>
2019-02-05 05:36:54 -0300

    add trailers model

---
+++ app/src/main/java/com/limamauricio/popularmovies/model/Trailer.java
@@ -0,0 +1,28 @@
+package com.limamauricio.popularmovies.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Trailer implements Serializable {
+
+
+    @Getter
+    @Setter
+    @SerializedName("id")
+    private String id;
+
+    @Getter
+    @Setter
+    @SerializedName("key")
+    private String key;
+
+    @Getter
+    @Setter
+    @SerializedName("name")
+    private String name;
+
+}
---
+++ app/src/main/java/com/limamauricio/popularmovies/ui/trailers/TrailerViewHolder.java
@@ -0,0 +1,4 @@
+package com.limamauricio.popularmovies.ui.movie;
+
+public class TrailerViewHolder {
+}
